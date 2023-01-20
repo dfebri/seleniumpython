e8e1a9b7d6af9e099f7c4cdcce8516be3dc0c622
dfebri <dwifebrimurcahyo@gmail.com>
2023-01-20 22:54:01 +0700

    repo

--- selenium.py
+++ selenium.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 import unittest
